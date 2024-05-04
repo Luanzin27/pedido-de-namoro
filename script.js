027c91dde642e782912a224b05d707cb1930644e
Luan <luan70918@gmail.com>
2024-05-04 15:21:52 -0300

    namoro

---
+++ script.js
@@ -0,0 +1,20 @@
+<script>
+                function fuja(){
+                    var botaoNao = document.getElementById("nao")
+        
+                    var larguraJanela = window.innerWidth;
+                    var alturaJanela = window.innerHeight;
+        
+                    var maxX = larguraJanela - botaoNao.offsetWidth;
+                    var maxY = alturaJanela - botaoNao.offsetHeight;
+        
+                    var aleatorioX = Math.floor(Math.random() * maxX);
+                    var aleatorioY = Math.floor(Math.random() * maxY);
+        
+                    botaoNao.style.left = aleatorioX + "px";
+                    botaoNao.style.top = aleatorioY + "px";
+                }
+                function parabens(){
+                    alert ("Paranbés, você fez a melhor escolha da sua vida :) ")
+                }
+            </script>
